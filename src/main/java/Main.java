4091146ab73ec8cab5fa6aff3ec295029f46451a
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2025-10-21 20:21:48 +0530

    single digit matching

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,31 +2,40 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     if (args.length != 2 || !args[0].equals("-E")) {
       System.out.println("Usage: ./your_program.sh -E <pattern>");
       System.exit(1);
     }
 
-    String pattern = args[1];  
+    String pattern = args[1];
     Scanner scanner = new Scanner(System.in);
     String inputLine = scanner.nextLine();
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.err.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    // 
+    //
     if (matchPattern(inputLine, pattern)) {
-        System.exit(0);
+      System.exit(0);
     } else {
-        System.exit(1);
+      System.exit(1);
     }
   }
 
   public static boolean matchPattern(String inputLine, String pattern) {
+    char[] inputLineArray = inputLine.toCharArray();
     if (pattern.length() == 1) {
       return inputLine.contains(pattern);
+    } else if (pattern.contains("\\d")) {
+      for (char ch : inputLineArray) {
+        if (Character.isDigit(ch)) {
+          return true;
+        }
+      }
+      return false;
     } else {
       throw new RuntimeException("Unhandled pattern: " + pattern);
     }
--- your_program.sh
+++ your_program.sh
@@ -22,3 +22,6 @@ set -e # Exit early if any commands fail
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
 exec java -jar /tmp/codecrafters-build-grep-java/codecrafters-grep.jar "$@"
+# exec java -jar /tmp/codecrafters-build-grep-java/codecrafters-grep.jar -E "\d" "apple123"
+
+
